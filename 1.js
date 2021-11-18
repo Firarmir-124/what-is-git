62edd1c0ec8a60d9c6cc83b35d8b0eebb8784135
Firarmir-124 <91172492+Firarmir-124@users.noreply.github.com>
2021-11-18 16:29:49 +0600

    JJJJ

--- 1.js
+++
@@ -1,8 +0,0 @@
-function addNumbers(a, b) {
-    let set = prompt('оператор: '),
-        a = prompt('Первое число: '),
-        b = prompt('Второе число: ');
-        if (set === '+') return a + b
-        else if (set === '-') return a - b
-        else console.log('Nan')
-}
